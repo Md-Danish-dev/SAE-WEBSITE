6f11f134007210dc5f0eccaced79ece2a28702fd
Alok <alokprajapati0303@gmail.com>
2025-09-21 15:12:01 +0530

    Added Project page and added font family

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/home/home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar'
 import AboutPage from './components/about/about'
+import Projectpage from './components/project/project'
 function App() {
   
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path='/about' element={<AboutPage />} />
+          <Route path='/project' element={<Projectpage />} />
         </Routes>
       </BrowserRouter>
     </>
